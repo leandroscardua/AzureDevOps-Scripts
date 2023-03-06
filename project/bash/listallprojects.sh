b986d8dbe98823e44dbcd1f501c8173e8ca48ee2
Leandro Scardua <leandroscar@gmail.com>
2023-03-06 21:38:05 +0000

    Update listallprojects.sh

--- project/bash/listallprojects.sh
+++ project/bash/listallprojects.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+#List all project in a Azure DevOps organization
+
+# https://dev.azure.com/xxxxxx"
+organization=
+
+az devops project list --org $organization --query "value[].name" -o tsv
