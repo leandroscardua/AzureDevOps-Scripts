b20942853d2f1ce08c733aa7cedc44c79da9f377
Leandro Scardua <leandroscar@gmail.com>
2023-03-06 21:37:54 +0000

    Delete listallprojects.sh

--- project/listallprojects.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#List all project in a Azure DevOps organization
-
-# https://dev.azure.com/xxxxxx"
-organization=
-
-az devops project list --org $organization --query "value[].name" -o tsv
