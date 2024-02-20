dadb19193a35cc1997fbbd70730c2e32a1fa7e2c
Leandro Scardua <leandroscar@gmail.com>
2024-02-20 22:09:56 +0000

    Create queuejobs.ps1

---
+++ analytics/pipeline/queuejobs.ps1
@@ -0,0 +1,13 @@
+# Add your PAT below
+$pat= ""
+$bytes = [System.Text.Encoding]::UTF8.GetBytes(":$($token)")
+$base64bytes = [System.Convert]::ToBase64String($bytes)
+$headers = @{ "Authorization" = "Basic $base64bytes"}
+
+$organization = ""
+# Select the desired ID, for the AzurePipeline use 9
+$poolid = ""
+
+$uri = "https://dev.azure.com/$organization/_apis/distributedtask/pools/$poolid/jobrequests?agentId=140&completeRequestCount=25"
+
+$result = Invoke-RestMethod -Uri $uri -Headers $headers -Method Get -ContentType "application/json"
